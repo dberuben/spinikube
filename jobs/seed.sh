1d8a6da6309d75c259ff2132ce1a97c28778ecb6
jeremy <jeremy.viel@ssense.com>
2016-08-24 14:49:48 -0400

    fix multi#2

--- jobs/seed.sh
+++ jobs/seed.sh
@@ -2,11 +2,23 @@
 cd /root
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 
-for job in front50 clouddriver rosco orca gate igor
-do
-    echo "building $job"
-	java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-$job --username jenkins --password jenkins -s &
-done
+echo "building deck\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-deck --username jenkins --password jenkins -s &
 
-echo "building deck"
-java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-deck --username jenkins --password jenkins -s
+echo "building front50\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-front50 --username jenkins --password jenkins -s &
+
+echo "building rosco\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-rosco --username jenkins --password jenkins -s &
+
+echo "building clouddriver\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-clouddriver --username jenkins --password jenkins -s &
+
+echo "building orca\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-orca --username jenkins --password jenkins -s &
+
+echo "building gate\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-gate --username jenkins --password jenkins -s &
+
+echo "building igor\n"
+java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-igor --username jenkins --password jenkins -s 
